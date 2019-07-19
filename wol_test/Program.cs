ddec329ebe43a16b2e33ed126a49b317e5b3fd96
Ji JaeMin <zzizzh@nate.com>
2019-07-19 11:09:56 +0900

    wol create

---
+++ wol_test/Program.cs
@@ -0,0 +1,151 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace wol_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prog= new Program();
+            // param : 목적 PC MAC주소 / 프로그램 실행 PC IP주소, / 프로그램 실행 PC 서브넷마스크
+            prog.WakeUp("00-23-81-2A-63-F4", "118.41.57.131", "255.255.255.128");
+        }
+
+        public void WakeUp(string macAddress, string ipAddress, string subnetMask)
+        {
+            UdpClient client = new UdpClient();
+
+            Byte[] datagram = new byte[102];
+
+            for (int i = 0; i <= 5; i++)
+            {
+                datagram[i] = 0xff;
+            }
+
+            string[] macDigits = null;
+            if (macAddress.Contains("-"))
+            {
+                macDigits = macAddress.Split('-');
+            }
+            else
+            {
+                macDigits = macAddress.Split(':');
+            }
+
+            if (macDigits.Length != 6)
+            {
+                throw new ArgumentException("Incorrect MAC address supplied!");
+            }
+
+            int start = 6;
+            for (int i = 0; i < 16; i++)
+            {
+                for (int x = 0; x < 6; x++)
+                {
+                    datagram[start + i * 6 + x] = (byte)Convert.ToInt32(macDigits[x], 16);
+                }
+            }
+
+            IPAddress address = IPAddress.Parse(ipAddress);
+            IPAddress mask = IPAddress.Parse(subnetMask);
+            IPAddress broadcastAddress = address.GetBroadcastAddress(mask);
+
+            client.Send(datagram, datagram.Length, broadcastAddress.ToString(), 3);
+        }
+    }
+
+    public static class SubnetMask
+    {
+        public static readonly IPAddress ClassA = IPAddress.Parse("255.0.0.0");
+        public static readonly IPAddress ClassB = IPAddress.Parse("255.255.0.0");
+        public static readonly IPAddress ClassC = IPAddress.Parse("255.255.255.0");
+
+        public static IPAddress CreateByHostBitLength(int hostpartLength)
+        {
+            int hostPartLength = hostpartLength;
+            int netPartLength = 32 - hostPartLength;
+
+            if (netPartLength < 2)
+                throw new ArgumentException("Number of hosts is to large for IPv4");
+
+            Byte[] binaryMask = new byte[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (i * 8 + 8 <= netPartLength)
+                    binaryMask[i] = (byte)255;
+                else if (i * 8 > netPartLength)
+                    binaryMask[i] = (byte)0;
+                else
+                {
+                    int oneLength = netPartLength - i * 8;
+                    string binaryDigit =
+                        String.Empty.PadLeft(oneLength, '1').PadRight(8, '0');
+                    binaryMask[i] = Convert.ToByte(binaryDigit, 2);
+                }
+            }
+            return new IPAddress(binaryMask);
+        }
+
+        public static IPAddress CreateByNetBitLength(int netpartLength)
+        {
+            int hostPartLength = 32 - netpartLength;
+            return CreateByHostBitLength(hostPartLength);
+        }
+
+        public static IPAddress CreateByHostNumber(int numberOfHosts)
+        {
+            int maxNumber = numberOfHosts + 1;
+
+            string b = Convert.ToString(maxNumber, 2);
+
+            return CreateByHostBitLength(b.Length);
+        }
+    }
+
+    public static class IPAddressExtensions
+    {
+        public static IPAddress GetBroadcastAddress(this IPAddress address, IPAddress subnetMask)
+        {
+            byte[] ipAdressBytes = address.GetAddressBytes();
+            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+
+            if (ipAdressBytes.Length != subnetMaskBytes.Length)
+                throw new ArgumentException("Lengths of IP address and subnet mask do not match.");
+
+            byte[] broadcastAddress = new byte[ipAdressBytes.Length];
+            for (int i = 0; i < broadcastAddress.Length; i++)
+            {
+                broadcastAddress[i] = (byte)(ipAdressBytes[i] | (subnetMaskBytes[i] ^ 255));
+            }
+            return new IPAddress(broadcastAddress);
+        }
+
+        public static IPAddress GetNetworkAddress(this IPAddress address, IPAddress subnetMask)
+        {
+            byte[] ipAdressBytes = address.GetAddressBytes();
+            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+
+            if (ipAdressBytes.Length != subnetMaskBytes.Length)
+                throw new ArgumentException("Lengths of IP address and subnet mask do not match.");
+
+            byte[] broadcastAddress = new byte[ipAdressBytes.Length];
+            for (int i = 0; i < broadcastAddress.Length; i++)
+            {
+                broadcastAddress[i] = (byte)(ipAdressBytes[i] & (subnetMaskBytes[i]));
+            }
+            return new IPAddress(broadcastAddress);
+        }
+
+        public static bool IsInSameSubnet(this IPAddress address2, IPAddress address, IPAddress subnetMask)
+        {
+            IPAddress network1 = address.GetNetworkAddress(subnetMask);
+            IPAddress network2 = address2.GetNetworkAddress(subnetMask);
+
+            return network1.Equals(network2);
+        }
+    }
+}
